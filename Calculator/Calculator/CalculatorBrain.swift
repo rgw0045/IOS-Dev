9497a971eae6ffccf238fb7c006c715e59b3acba
Ricky <ricky@Ricardos-MacBook-Pro.local>
2017-04-15 23:47:31 -0500

    Binary operations
    Trying to add binary operations but currently has a set fault

--- Calculator/Calculator/CalculatorBrain.swift
+++ Calculator/Calculator/CalculatorBrain.swift
@@ -8,6 +8,14 @@
 
 import Foundation
 
+func changeSign(operand: Double) -> Double {
+    return -operand
+}
+
+func multiply(op1: Double, op2: Double) -> Double {
+    return op1 * op2
+}
+
 struct CalculatorBrain {
     
     private var accumulator: Double?
@@ -15,6 +23,8 @@ struct CalculatorBrain {
     private enum operation {
         case constant(Double)
         case unaryOperation((Double) -> Double)
+        case binaryOperation((Double,Double) -> Double)
+        case equals
     }
     
     private var Operations: Dictionary<String, operation> =
@@ -22,24 +32,54 @@ struct CalculatorBrain {
         "π" : operation.constant(Double.pi),
         "e" : operation.constant(M_E),
         "√" : operation.unaryOperation(sqrt),
-        "cos" : operation.unaryOperation(cos)
+        "cos" : operation.unaryOperation(cos),
+        "±" : operation.unaryOperation(changeSign),
+        "×" : operation.binaryOperation(multiply),
+        "=" : operation.equals
     ]
     
     mutating func PerformOperation(_ symbol: String){
-        //because it may not be in the table to it will be an optional
         if let operation = Operations[symbol] {
             switch operation {
             case .constant(let value) :
                 accumulator = value
-            case .unaryOperation :
-                break
+            case .unaryOperation(let function) :
+                if accumulator != nil{
+                    accumulator = function(accumulator!)
+                }
+            case .binaryOperation(let function) :
+                if accumulator != nil {
+                    PendingBinaryOperation = PendingBinaryOperation(function: function, firstOperand: accumulator!)
+                    accumulator = nil
+                }
+            case .equals :
+                PerformPendingBinaryOperation()
             }
+        }
+    }
+    
+    private mutating func PerformPendingBinaryOperation() {
+        if PendingBinaryOperation != nil && accumulator != nil{
+            accumulator = PendingBinaryOperation!.perform(with: accumulator!)
+            PendingBinaryOperation = nil
+        }
+    }
+    
+    private var PendingBinaryOperation: PendingBinaryOperation?
+    
+    private struct PendingBinaryOperation {
+        let function: (Double,Double) -> Double
+        let firstOperand: Double
+        
+        func perform(with secondOperand: Double) -> Double{
+            return function(firstOperand, secondOperand)
         }
     }
     
     mutating func SetOperand(_ operand: Double){
         accumulator = operand
     }
+    
     
     var result: Double? {
         get {
---
+++ Calculator/CalculatorPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+func changeSign(operand: Double) -> Double{
+    return -operand
+}
+
+let i = 27
+var f: (Double) -> Double
+f = changeSign(operand:)
+let x = f(81)
