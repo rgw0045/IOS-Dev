01662fa2c18cd2f3f5e20bf3a773dfe6f2b904e6
rgw0045 <ricky.wiggins91@gmail.com>
2018-01-07 23:43:07 -0600

    Dice Roller App
    Started the tutorial for the dice Roller app

---
+++ Dice Roller/Dice Roller/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Dice Roller
+//
+//  Created by Ricardo Wiggins on 1/7/18.
+//  Copyright © 2018 Ricardo Wiggins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+    }
+    
+}
+
