3f23a4036c748444b25f2411cb013f0d50a8ac91
rgw0045 <ricky.wiggins91@gmail.com>
2018-01-09 22:53:29 -0600

    Dice App update
    Finished the dice app for iPhone 6

--- Dice Roller/Dice Roller/ViewController.swift
+++ Dice Roller/Dice Roller/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
 
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
@@ -16,6 +21,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        UpdateDiceImage()
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,7 +30,19 @@ class ViewController: UIViewController {
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+        UpdateDiceImage()
     }
     
+    func UpdateDiceImage(){
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        UpdateDiceImage();
+    }
 }
 
